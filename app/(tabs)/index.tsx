748d8aca8e58b3d345674478c80f5875e4744c82
zxshhhh <zhoulopagalan13@gmail.com>
2025-08-29 10:30:06 +0800

    Screenshot

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -17,8 +17,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
+        <ThemedText type="title">Welcome, Zhoulo Pagalan! <HelloWave /></ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
